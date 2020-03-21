e6c4b981adeee011e161086eaf40e2c533f39d84
Tom Diggelmann <mail@tomdgm.ch>
2020-03-21 17:48:16 +0100

    lint

--- husky.config.js
+++ husky.config.js
@@ -2,6 +2,6 @@ module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': 'yarn lint',
-    'pre-push': 'yarn lint'
-  }
+    'pre-push': 'yarn lint',
+  },
 }
