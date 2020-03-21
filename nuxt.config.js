1e3885e6955ceb46867464f2cbe91f7639604977
Nattha <natthakit.khamso@gmail.com>
2020-03-21 02:31:12 +0100

    pass an enabled flag as an module option

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,12 @@ export default {
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
-    '~/plugins/module',
+    [
+      '~/plugins/module',
+      {
+        enabled: true,
+      },
+    ],
   ],
   /*
    ** Nuxt.js modules
--- plugins/module.js
+++ plugins/module.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path')
 
 export default function nuxtBreaky(moduleOptions) {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' && moduleOptions.enabled) {
     this.addPlugin({
       src: resolve(__dirname, 'plugin.js'),
       ssr: false,
