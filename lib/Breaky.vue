2d219a1d6b870fc7eb070d9e9779fc91e90052a0
Nattha <natthakit.khamso@gmail.com>
2020-03-21 23:50:58 +0100

    install interactjs

--- lib/Breaky.vue
+++ lib/Breaky.vue
@@ -72,6 +72,7 @@
 
 <script>
 import throttle from 'lodash/throttle'
+import interact from 'interactjs'
 import TransitionExpand from './TransitionExpand'
 import { theme } from '~/tailwind.config'
 
@@ -154,6 +155,8 @@ export default {
     this.$on('hook:beforeDestroy', () => {
       window.removeEventListener('resize', this.resizeHandler)
     })
+
+    this.initInteract()
   },
 
   methods: {
@@ -182,6 +185,61 @@ export default {
       // set the found breakpoint
       this.currentBreakpoint = this.sortedBreakpoints[this.foundBreakpoint - 1]
     }, 100),
+
+    initInteract() {
+      // target elements with the "draggable" class
+      interact('.card').draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        modifiers: [
+          interact.modifiers.restrict({
+            restriction: 'self',
+            endOnly: true,
+          }),
+        ],
+        // enable autoScroll
+        autoScroll: true,
+
+        listeners: {
+          // call this function on every dragmove event
+          move: this.dragMoveListener,
+
+          // call this function on every dragend event
+          end(event) {
+            const textEl = event.target.querySelector('p')
+
+            textEl &&
+              (textEl.textContent =
+                'moved a distance of ' +
+                Math.sqrt(
+                  ((event.pageX - event.x0) ** 2 +
+                    (event.pageY - event.y0) ** 2) |
+                    0
+                ).toFixed(2) +
+                'px')
+          },
+        },
+      })
+    },
+
+    /**
+     * Move the card
+     */
+    dragMoveListener(event) {
+      const target = event.target
+      // keep the dragged position in the data-x/data-y attributes
+      const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+      const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+      // translate the element
+      target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)'
+
+      // update the posiion attributes
+      target.setAttribute('data-x', x)
+      target.setAttribute('data-y', y)
+    },
   },
 }
 </script>
