bcdd24cde48d0ff5b04645fe681b80fb1882481a
Tom Diggelmann <mail@tomdgm.ch>
2020-03-21 18:01:22 +0100

    lint

--- test/module.test.js
+++ test/module.test.js
@@ -4,7 +4,7 @@ describe('module', () => {
   let nuxt
 
   beforeAll(async () => {
-    ({ nuxt } = (await setup(loadConfig(__dirname, '../../example'))))
+    ;({ nuxt } = await setup(loadConfig(__dirname, '../../example')))
   }, 60000)
 
   afterAll(async () => {
