8933d83033a9a98da6d813151297d8e9c97c3490
Tom Diggelmann <mail@tomdgm.ch>
2020-03-22 13:55:16 +0100

    fix scale on mobile

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,6 +1,12 @@
 const { resolve } = require('path')
 
 module.exports = {
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
