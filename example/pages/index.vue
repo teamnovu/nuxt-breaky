e0115ccffce93c18c667927c1c316fa48dba69b8
Nattha <natthakit.khamso@gmail.com>
2020-03-22 13:04:41 +0100

    add random unsplash image

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-    Breaky
+  <div
+    class="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center"
+    :style="randomUnsplashImage"
+  >
+    <h1 class="text-white font-bold text-5xl">
+      breaky
+    </h1>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    randomUnsplashImage() {
+      return {
+        backgroundImage: 'url(https://source.unsplash.com/random/1600x900)',
+      }
+    },
+  },
+}
 </script>
