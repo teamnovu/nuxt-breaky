c3e64991cfb32795d62708d512e992b3aec0ec0b
Tom Diggelmann <mail@tomdgm.ch>
2020-03-23 22:17:23 +0100

    refactor shorter vue plugin registration

--- example/vue/main.js
+++ example/vue/main.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import '../assets/css/tailwind.css'
 import resolveConfig from 'tailwindcss/resolveConfig'
 import Breaky from '../../lib/plugin-vue'
-import twd from '../tailwind.config'
 import App from './App.vue'
 
 Vue.use(Breaky, {
-  tailwindConfig: resolveConfig(twd),
+  tailwindConfig: resolveConfig(require('../tailwind.config')),
   enabled: true,
   enableInProd: process.env.DEPLOY_ENV === 'GH_PAGES',
   position: 'bottomRight',
